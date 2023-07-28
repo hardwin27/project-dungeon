203cd0943d24b86da9c17f68c4acdac9e2b70711
HaruEru <hardwin27@gmail.com>
2023-07-28 09:02:39 +0700

    Progress on CharacterAi. Ai trying to detect the enemy

--- Assets/Scripts/AI/State/StateChase.cs
+++ Assets/Scripts/AI/State/StateChase.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateChase : MonoBehaviour
+public class StateChase : IState
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnEnter()
     {
-        
+        throw new System.NotImplementedException();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnExit()
     {
-        
+        throw new System.NotImplementedException();
+    }
+
+    public void Tick()
+    {
+        throw new System.NotImplementedException();
     }
 }
--- Assets/Scripts/Character/CharacterAI.cs
+++ Assets/Scripts/Character/CharacterAI.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class CharacterAiData
+{
+    [SerializeField] private GameObject _aiTarget = null;
+
+    public GameObject AiTarget { set => _aiTarget = value; get => _aiTarget; }
+}
+
 public class CharacterAi : MonoBehaviour
 {
     [SerializeField] private CharacterMovement _characterMovement;
     [SerializeField] private CharacterVisual _characterVisual;
     [SerializeField] private CharacterCombat _characterCombat;
+
+    [SerializeField] private CharacterAiData _aiData = new CharacterAiData();
+
+    [SerializeField] private LayerMask _targetLayerMask;
+    [SerializeField] private float _targetDetectionRange;
     
     private StateMachine _stateMachine;
 
@@ -15,10 +28,28 @@ public class CharacterAi : MonoBehaviour
         InitiateStateMachine();
     }
 
-    private void Update() => _stateMachine.Tick();
+    private void Update()
+    {
+        DetectTarget();
+
+        _stateMachine.Tick();
+    }
 
     private void InitiateStateMachine()
     {
         _stateMachine = new StateMachine();
+    }
+
+    private void DetectTarget()
+    {
+        Collider2D targetCollider = Physics2D.OverlapCircle(_characterVisual.transform.position, _targetDetectionRange, _targetLayerMask);
+        if (targetCollider != null)
+        {
+            _aiData.AiTarget = (targetCollider.attachedRigidbody == null) ? targetCollider.gameObject : targetCollider.attachedRigidbody.gameObject;
+        }
+        else
+        {
+            _aiData.AiTarget = null;
+        }
     }
 }
